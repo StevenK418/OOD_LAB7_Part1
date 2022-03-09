6e4c00ad3e6d967c5e17ab50098a14dc9eef32ea
Steven Kelly <stevenkelly418@gmail.com>
2022-03-09 10:49:05 +0000

    => Added all code for Exercise 3.

--- Lab7_Part1/Lab7_Part1_Ex1/MainWindow.xaml.cs
+++ Lab7_Part1/Lab7_Part1_Ex1/MainWindow.xaml.cs
@@ -57,5 +57,27 @@ namespace Lab7_Part1_Ex1
 
             Ex2TblkCount.Text = results.Count.ToString();
         }
+
+        private void Ex3Button_Click(object sender, RoutedEventArgs e)
+        {
+            //return the total number of orders for product 7
+            var queryl = 
+                from detail in db.Order_Details
+                where detail.ProductID == 7
+                select detail;
+            //return the total value of orders for product 7
+            var query2 = from detail in db.Order_Details
+                where detail.ProductID == 7
+                select detail.UnitPrice * detail.Quantity;
+            int numberOforders = queryl.ToList().Count;
+            decimal totalValue = query2.Sum();
+            decimal averageValue = query2.Average();
+            Ex3TblkDetails.Text = string.Format("Total number of orders {0}\nValue of Orders {1:C}\nAverage Order Value {2:C}", numberOforders, totalValue, averageValue);
+        }
+
+        private void Ex4Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
